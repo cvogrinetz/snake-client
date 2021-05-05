31f9847e6f6272b68a6abc1ee41a417e8f69826d
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-05-05 20:42:50 +0000

    Refractored play.js and moved input function into input.js

--- play.js
+++ play.js
@@ -1,22 +1,7 @@
 const { connect, net } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 connect();
-
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  const handleUserInput = (key) => {
-    if(key === '\u0003') {
-      process.exit();
-    }
-  };
-  stdin.on('data', handleUserInput)
-  
-  return stdin;
-};
 
 setupInput();
