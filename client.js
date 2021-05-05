287e62c96679f2f1e29519e0ef5a0fbec3fc0c17
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-05-05 20:17:48 +0000

    Tested the moving of snek upon connect with setTimeout and setInterval

--- client.js
+++ client.js
@@ -13,7 +13,14 @@ const connect = () => {
 
   conn.on('connect', () => {
     console.log('Connection successfully established to game server');
-    conn.write('Name: 357')
+    conn.write('Name: 357');
+    // conn.write('Move: up');
+    // setTimeout(() => {
+    //   conn.write('Move: up');   // MOVE SNEK AUTOMATICALLY UPON CONNECT
+    // }, 1000);
+    // setInterval(() => {
+    //   conn.write('Move: left');
+    // }, 2000);
   })
 
 
@@ -25,3 +32,9 @@ module.exports = {
   connect,
   net
 };
+
+
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
