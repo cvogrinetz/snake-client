55ba8fe9ee2e25c37cb398c1f4785f3718c3edd1
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-05-05 20:37:30 +0000

    Added userInput with stdin. ctrl-c now exits game

--- play.js
+++ play.js
@@ -2,3 +2,21 @@ const { connect, net } = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = (key) => {
+    if(key === '\u0003') {
+      process.exit();
+    }
+  };
+  stdin.on('data', handleUserInput)
+  
+  return stdin;
+};
+
+setupInput();
