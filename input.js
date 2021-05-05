ceb35ac5ff59458db005bbd5b407fce404370c41
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-05-05 21:09:34 +0000

    Added connection capability inside input.js so it can interact with game server. Updated handleUserInput to add movement to snek

--- input.js
+++ input.js
@@ -1,4 +1,10 @@
-const setupInput = () => {
+let connection;
+
+
+const setupInput = (conn) => {
+
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -7,6 +13,14 @@ const setupInput = () => {
   const handleUserInput = (key) => {
     if(key === '\u0003') {
       process.exit();
+    } else if (key === 'w') {
+      conn.write('Move: up');
+    } else if (key === 's') {
+      conn.write('Move: down');
+    } else if (key === 'a') {
+      conn.write('Move: left');
+    } else if (key === 'd') {
+      conn.write('Move: right');
     }
   };
   stdin.on('data', handleUserInput)
